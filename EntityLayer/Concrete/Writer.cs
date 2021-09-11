49ae993a5b80932362ea73dcf061834f1aba8e8f
Stephantouchh <81231717+Stephantouchh@users.noreply.github.com>
2021-09-11 23:58:40 +0300

    deneme

--- EntityLayer/Concrete/Writer.cs
+++ EntityLayer/Concrete/Writer.cs
@@ -16,6 +16,6 @@ namespace EntityLayer.Concrete
         public string WriterImage { get; set; }
         public string WriterMail { get; set; }
         public string WriterPassword { get; set; }
-        public bool WriterStatus { get; set; }
+        public bool WriterStatus { get; set; } /*merhaba*/
     }
 }
