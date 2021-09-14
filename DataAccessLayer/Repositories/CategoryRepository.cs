82f7b67c061fc868ea43895022e39e62ffd10e5c
Stephantouchh <81231717+Stephantouchh@users.noreply.github.com>
2021-09-14 19:36:06 +0300

    14.09.2021 16. Ders başarıyla tamamlandı.

---
+++ DataAccessLayer/Repositories/CategoryRepository.cs
@@ -0,0 +1,44 @@
+﻿using DataAccessLayer.Abstract;
+using DataAccessLayer.Concrete;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Repositories
+{
+    public class CategoryRepository : ICategoryDal
+    {
+        Context c = new Context();
+
+        public void AddCategory(Category category)
+        {
+            c.Add(category);
+            c.SaveChanges();
+        }
+
+        public void DeleteCategory(Category category)
+        {
+            c.Remove(category);
+            c.SaveChanges();
+        }
+
+        public Category GetById(int id)
+        {
+            return c.Categories.Find(id);
+        }
+
+        public List<Category> ListAllCategory()
+        {
+            return c.Categories.ToList();
+        }
+
+        public void UpdateCategory(Category category)
+        {
+            c.Update(category);
+            c.SaveChanges();
+        }
+    }
+}
