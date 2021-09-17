b8b26b323923697913c3e42b30b0fcccd6e45712
Stephantouchh <81231717+Stephantouchh@users.noreply.github.com>
2021-09-17 20:14:49 +0300

    17.09.2021 20. Ders başarıyla tamamlandı.

---
+++ BusinessLayer/Concrete/CategoryManager.cs
@@ -0,0 +1,45 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class CategoryManager : ICategoryService
+    {
+        EfCategoryRepository repo;
+
+        public CategoryManager()
+        {
+            repo = new EfCategoryRepository();
+        }
+        public void CategoryAdd(Category category)
+        {
+            repo.Insert(category);
+        }
+
+        public void CategoryDelete(Category category)
+        {
+            repo.Delete(category);
+        }
+
+        public void CategoryUpdate(Category category)
+        {
+            repo.Update(category);
+        }
+
+        public Category GetById(int id)
+        {
+            return repo.GetByID(id);
+        }
+
+        public List<Category> GetList()
+        {
+            return repo.GetListAll();
+        }
+    }
+}
