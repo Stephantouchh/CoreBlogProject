5a23a6ddacc5c076bab03f6f49583584294c34ac
Stephantouchh <81231717+Stephantouchh@users.noreply.github.com>
2021-10-03 16:01:40 +0300

    03.10.2021 Tüm Ödevler Yapıldı.

--- BusinessLayer/ValidationRules/WriterValidator.cs
+++ BusinessLayer/ValidationRules/WriterValidator.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace BusinessLayer.ValidationRules
@@ -17,6 +18,19 @@ namespace BusinessLayer.ValidationRules
             RuleFor(x => x.WriterPassword).NotEmpty().WithMessage("Şifre boş geçilemez!");
             RuleFor(x => x.WriterName).MinimumLength(2).WithMessage("Lütfen en az 2 karakterlik veri girişi yapınız!");
             RuleFor(x => x.WriterName).MaximumLength(50).WithMessage("Lütfen en fazla 50 karakterlik veri girişi yapınız!");
+            RuleFor(x => x.WriterPassword).Must(IsPasswordValid).WithMessage("Parolanızda en az bir küçük harf bir büyük harf ve rakam olmalıdır!");
+        }
+        private bool IsPasswordValid(string arg)
+        {
+            try
+            {
+                Regex regex = new Regex(@"^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[0-9])[A-Za-z\d]");
+                return regex.IsMatch(arg);
+            }
+            catch
+            {
+                return false;
+            }
         }
     }
 }
--- DotNetCoreCamp/Controllers/RegisterController.cs
+++ DotNetCoreCamp/Controllers/RegisterController.cs
@@ -4,6 +4,7 @@ using DataAccessLayer.EntityFramework;
 using EntityLayer.Concrete;
 using FluentValidation.Results;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -18,28 +19,49 @@ namespace DotNetCoreCamp.Controllers
         [HttpGet]
         public IActionResult Index()
         {
+            ViewBag.Cities = GetCityList();
             return View();
         }
         [HttpPost]
-        public IActionResult Index(Writer writer)
+        public IActionResult Index(Writer writer,string PasswordAgain, string cities)
         {
             WriterValidator wv = new WriterValidator();
             ValidationResult results = wv.Validate(writer);
-            if (results.IsValid)
+            if (results.IsValid && writer.WriterPassword==PasswordAgain)
             {
                 writer.WriterStatus = true;
                 writer.WriterAbout = "Deneme Test";
                 wm.WriterAdd(writer);
                 return RedirectToAction("Index", "Blog");
             }
-            else
+            else if(!results.IsValid)
             {
                 foreach (var item in results.Errors)
                 {
                     ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
                 }
             }
+            else
+            {
+                ModelState.AddModelError("WriterPassword", "Girdiğiniz Şifreler Eşleşmiyor! Lütfen Tekrar Deneyiniz");
+            }
             return View();
+        }
+
+        public List<SelectListItem> GetCityList()
+        {
+            List<SelectListItem> adminrole = (from x in GetCity()
+                                              select new SelectListItem
+                                              {
+                                                  Text = x,
+                                                  Value = x
+                                              }).ToList();
+            return adminrole;                  
+        }
+        public List<string> GetCity()
+        {
+            String[] CitiesArray = new String[] { "Adana", "Adıyaman", "Afyon", "Ağrı", "Aksaray", "Amasya", "Ankara", "Antalya", "Ardahan", "Artvin", "Aydın", "Bartın", "Batman", "Balıkesir", "Bayburt", "Bilecik", "Bingöl", "Bitlis", "Bolu", "Burdur", "Bursa", "Çanakkale", "Çankırı", "Çorum", "Denizli", "Diyarbakır", "Düzce", "Edirne", "Elazığ", "Erzincan", "Erzurum", "Eskişehir", "Gaziantep", "Giresun", "Gümüşhane", "Hakkari", "Hatay", "Iğdır", "Isparta", "İçel", "İstanbul", "İzmir", "Karabük", "Karaman", "Kars", "Kastamonu", "Kayseri", "Kırıkkale", "Kırklareli", "Kırşehir", "Kilis", "Kocaeli", "Konya", "Kütahya", "Malatya", "Manisa", "Kahramanmaraş", "Mardin", "Muğla", "Muş", "Nevşehir", "Niğde", "Ordu", "Osmaniye", "Rize", "Sakarya", "Samsun", "Siirt", "Sinop", "Sivas", "Tekirdağ", "Tokat", "Trabzon", "Tunceli", "Şanlıurfa", "Şırnak", "Uşak", "Van", "Yalova", "Yozgat", "Zonguldak" };
+            return new List<string>(CitiesArray);
         }
     }
 }
