94c44061227f00ac012a4508e4db53208b13d82f
Stephantouchh <81231717+Stephantouchh@users.noreply.github.com>
2021-10-05 20:30:05 +0300

    05.10.2021 42. Ders başarıyla tamamlandı. Ödevler Geliyor...

---
+++ DotNetCoreCamp/Controllers/AboutController.cs
@@ -0,0 +1,25 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DotNetCoreCamp.Controllers
+{
+    public class AboutController : Controller
+    {
+        AboutManager abm = new AboutManager(new EfAboutRepository());
+
+        public IActionResult Index()
+        {
+            var values = abm.GetList();
+            return View(values);
+        }
+        public PartialViewResult SocialMediaAbout()
+        {
+            return PartialView();
+        }
+    }
+}
---
+++ DotNetCoreCamp/Controllers/ContactController.cs
@@ -0,0 +1,30 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DotNetCoreCamp.Controllers
+{
+    public class ContactController : Controller
+    {
+        ContactManager cm = new ContactManager(new EfContactRepository());
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(Contact p)
+        {
+            p.ContactDate = DateTime.Parse(DateTime.Now.ToShortDateString());
+            p.ContactStatus = true;
+            cm.ContactAdd(p);
+            return RedirectToAction("Index","Blog");
+        }
+    }
+}
