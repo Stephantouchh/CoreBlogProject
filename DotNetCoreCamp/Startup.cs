371c636268cb246b6341d70486d92b3dec96791f
Stephantouchh <81231717+Stephantouchh@users.noreply.github.com>
2021-11-23 19:27:01 +0300

    23.11.2021 90. Ders başarıyla tamamlandı.

--- DotNetCoreCamp/Startup.cs
+++ DotNetCoreCamp/Startup.cs
@@ -84,8 +84,14 @@ namespace DotNetCoreCamp
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
-                    name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                name: "areas",
+                pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+              );
+
+
+                endpoints.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
