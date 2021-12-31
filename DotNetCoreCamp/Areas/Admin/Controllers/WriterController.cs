5d4a254c5144f579a63b0c175e6682f365204a38
Stephantouchh <81231717+Stephantouchh@users.noreply.github.com>
2021-12-31 13:37:46 +0300

    31.12.2021 109. Ders başarıyla tamamlandı.

---
+++ DotNetCoreCamp/Areas/Admin/Controllers/WriterController.cs
@@ -0,0 +1,52 @@
+﻿using DotNetCoreCamp.Areas.Admin.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DotNetCoreCamp.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class WriterController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        public IActionResult WriterList()
+        {
+            var jsonwriters = JsonConvert.SerializeObject(writers);
+            return Json(jsonwriters);
+        }
+
+        public IActionResult GetWriterByID(int writerid)
+        {
+            var findWriter = writers.FirstOrDefault(x => x.Id == writerid);
+            var jsonWriters = JsonConvert.SerializeObject(findWriter);
+            return Json(jsonWriters);
+        }
+
+        public static List<WriterClass> writers = new List<WriterClass>
+        {
+            new WriterClass
+            {
+                Id=1,
+                Name="Ayşe"
+            },
+            new WriterClass
+            {
+                Id=2,
+                Name="Ahmet"
+            },
+            new WriterClass
+            {
+                Id=3,
+                Name="Buse"
+            }
+        };
+    }
+}
