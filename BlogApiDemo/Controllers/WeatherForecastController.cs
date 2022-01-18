f25b388c2afd1d562414bfe793c2ef51d24576e7
Stephantouchh <81231717+Stephantouchh@users.noreply.github.com>
2022-01-18 21:57:09 +0300

    18.01.2022 119. Ders başarıyla tamamlandı.

--- BlogApiDemo/Controllers/WeatherForecastController.cs
+++ BlogApiDemo/Controllers/WeatherForecastController.cs
@@ -26,12 +26,12 @@ namespace BlogApiDemo.Controllers
         [HttpGet]
         public IEnumerable<WeatherForecast> Get()
         {
-            var rng = new Random();
+            var rnd = new Random();
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateTime.Now.AddDays(index),
-                TemperatureC = rng.Next(-20, 55),
-                Summary = Summaries[rng.Next(Summaries.Length)]
+                TemperatureC = rnd.Next(-20, 55),
+                Summary = Summaries[rnd.Next(Summaries.Length)]
             })
             .ToArray();
         }
