b986500af7352fbb4983fcd041a06fefaf791dd6
Stephantouchh <81231717+Stephantouchh@users.noreply.github.com>
2022-02-20 03:31:53 +0300

    20.02.2022 UpdateAll

--- DataAccessLayer/Concrete/Context.cs
+++ DataAccessLayer/Concrete/Context.cs
@@ -1,4 +1,5 @@
 ﻿using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -8,7 +9,7 @@ using System.Threading.Tasks;
 
 namespace DataAccessLayer.Concrete
 {
-    public class Context : DbContext
+    public class Context : IdentityDbContext<AppUser>
     {
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
@@ -41,6 +42,7 @@ namespace DataAccessLayer.Concrete
                 .HasForeignKey(z => z.ReceiverID)
                 .OnDelete(DeleteBehavior.ClientSetNull);
 
+            base.OnModelCreating(modelBuilder);
             //HomeMatches-->WriterSender
             //AwayMatches-->WriterReceiver
 
